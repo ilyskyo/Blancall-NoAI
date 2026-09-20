f33253cbe90958350794c81650a57a0c827da774
ilyskyo <205641139+ilyskyo@users.noreply.github.com>
2026-09-20 23:39:40 +0800

    release: 7.0.1 (versionCode 33)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ilyskyo.blancall.noai"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "7.0-NoAI"
+        versionCode = 33
+        versionName = "7.0.1-NoAI"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
